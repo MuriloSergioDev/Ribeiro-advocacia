8f671a22b609f11c6b5695bb8121b44d2fea7074
Murilo Sergio Cardoso do Carmo <msccarmo2@gmail.com>
2023-10-07 18:57:00 -0300

    bug fixes

--- src/components/BenefitsSection/index.tsx
+++ src/components/BenefitsSection/index.tsx
@@ -1,9 +1,7 @@
-import { useInView } from 'react-intersection-observer'
 import BenefitOne from './BenefitOne'
 import BenefitTwo from './BenefitTwo'
 
 function BenefitsSection() {
-  const [ref, inView] = useInView({ threshold: 0.3, triggerOnce: true })
   return (
     <>
       <div className="w-full bg-main-color causas" id="vantagens">
